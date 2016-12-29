39851383b9629ef245084efa60aa7e7e11a9ceee
House <House@DESKTOP-TGDE2N5>
2016-12-28 18:57:43 -0600

    Add project files.

---
+++ VowelCounter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Enter a word and I will count the number of vowels.");
+            string strWord = Console.ReadLine().ToUpper();
+            string[] vowels = { "A", "E", "I", "O", "U" };
+            int count = 0;
+
+            foreach (string t in vowels) 
+                {
+                
+                            if(strWord.Contains(t))
+                                {
+                                    count += 1;
+                                }
+                    
+                }
+            
+            Console.WriteLine("There are " + count.ToString() + " vowels in your word!");
+            Console.ReadKey();
+        }
+    }
+}
